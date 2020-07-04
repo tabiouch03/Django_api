ae321121fbab30e5d1899c1ac54482d6b6ab2dd5
tabiouch03 <guerreiro.fabio@outlook.fr>
2020-07-04 06:19:22 +0200

    add comment directive

--- angular/src/app/border-card.directive.ts
+++ angular/src/app/border-card.directive.ts
@@ -1,5 +1,9 @@
 import { Directive, ElementRef, HostListener, Host } from '@angular/core';
 
+
+// Cette directive aura pour effet de changer la bordure d'une carte 
+// lors du passage de la souris.
+// Ajouter simplement gameBorderCard dans une balise HTML pour l'utiliser
 @Directive({ 
   selector: '[gameBorderCard]' 
 })
